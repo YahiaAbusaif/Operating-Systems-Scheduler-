89e51b6ff9088493b97b34c59e811ed310befff1
Yahia Abusaif <Y_A_Abusaif2014@hotmail.com>
2018-11-27 11:24:49 +0200

    old

---
+++ Part2_Scheduler Module/Source Code/OLD Source Code/OLD Source Code/OS2/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace OS2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Algo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Context = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FileName = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.output = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(848, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 17);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "scheduling algorithms";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1437, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 41);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Algo
+            // 
+            this.Algo.FormattingEnabled = true;
+            this.Algo.Location = new System.Drawing.Point(999, 19);
+            this.Algo.Name = "Algo";
+            this.Algo.Size = new System.Drawing.Size(167, 24);
+            this.Algo.TabIndex = 16;
+            this.Algo.SelectedIndexChanged += new System.EventHandler(this.Algo_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(568, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 17);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Time Quantum";
+            // 
+            // Time
+            // 
+            this.Time.Location = new System.Drawing.Point(675, 23);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(167, 22);
+            this.Time.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(271, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Context Switching";
+            // 
+            // Context
+            // 
+            this.Context.Location = new System.Drawing.Point(395, 23);
+            this.Context.Name = "Context";
+            this.Context.Size = new System.Drawing.Size(167, 22);
+            this.Context.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "file name";
+            // 
+            // FileName
+            // 
+            this.FileName.Location = new System.Drawing.Point(83, 23);
+            this.FileName.Name = "FileName";
+            this.FileName.Size = new System.Drawing.Size(167, 22);
+            this.FileName.TabIndex = 10;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 72);
+            this.chart1.Name = "chart1";
+            series1.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.TopBottom;
+            series1.BackImageTransparentColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series1.BackSecondaryColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            series1.BorderColor = System.Drawing.Color.Red;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "process";
+            series1.ShadowColor = System.Drawing.Color.Lime;
+            series1.YValuesPerPoint = 4;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1498, 571);
+            this.chart1.TabIndex = 19;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1183, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 17);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "output path";
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(1269, 25);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(148, 22);
+            this.output.TabIndex = 21;
+            this.output.TextChanged += new System.EventHandler(this.output_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1546, 690);
+            this.Controls.Add(this.Algo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Context);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FileName);
+            this.Name = "Form1";
+            this.Text = "Scheduler Module";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox Algo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Time;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Context;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FileName;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ Part2_Scheduler Module/Source Code/OLD Source Code/OLD Source Code/OS2/Form1.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OS2
+{
+    public partial class Form1 : Form
+    {
+        public class Process
+        {
+            public int ID, priority;
+            public Double arrivalTime, burstTime, WaitingTime, FinishTime, ReminingTime;
+        }; //save all data for each process
+        public class slides
+        {
+            public int ID;
+            public Double start, end;
+        }; //save the id of the process that run on cpu from start to end
+        public List<Process> Mylist; //list of process
+        List<slides> Final; //list of slides time 
+        public int number; //number of process
+        public Double TimeQuantum; //For RR
+        public Double step=0.001; // for Double number differance that will be ignored
+        public Double ContextSwitching; // time for switch from process to another -> assume if process ends before switch to another then take 0 time 
+        
+        public Form1() //initialize 
+        {
+            InitializeComponent();
+            Algo.Items.Add("HPF");
+            Algo.Items.Add("FCFS");
+            Algo.Items.Add("RR");
+            Algo.Items.Add("SRTN");
+            
+        }
+
+        public Double PTQ(Double time) //add ContextSwitching slides between the 2 process
+        {
+            if (Final == null)
+                return time;
+            slides nslid2 = new slides();
+            nslid2.ID = 0;
+            nslid2.start = time;
+            time += ContextSwitching;
+            nslid2.end = time;
+            Final.Add(nslid2);
+            return time;
+        }
+        public void HPF() //algorithm Hogh piority first
+        {
+            if (Final == null || Mylist == null)
+                return;
+            int count = 0,mx,last=-1;
+            Double time = 0.0,mn;
+            while (count < Mylist.Count) //while number of finish process != number of total process 
+            {
+                mn = 10000.0;
+                mx = -1;
+                //search on maximum priorty 
+                for (int i = 0; i < number; i++)
+                {
+                    if (Mylist[i].FinishTime >step) continue;
+                    if (Mylist[i].arrivalTime > time)
+                    {
+                        mn = Mylist[i].arrivalTime;
+                        break;
+                    } 
+                    if (mx == -1)
+                        mx = i;
+                    else if (Mylist[mx].priority < Mylist[i].priority)
+                        mx = i;
+                }
+                if (mx == -1) //if no process has arrive yet add 0 to minimum arrival time 
+                {
+                    slides nslide = new slides();
+                    nslide.ID = 0;
+                    nslide.start = time;
+                    nslide.end = mn;
+                    time = mn;
+                    Final.Add(nslide);
+                    last = -1;
+                }
+                else //cpu will run highest priorty untill it finish or new process arrive
+                {
+                    if (last != mx && last != -1)
+                        time = PTQ(time);
+                    slides nslide = new slides();
+                    nslide.ID =Mylist[mx].ID;
+                    nslide.start = time;
+                    if (Mylist[mx].burstTime - Mylist[mx].ReminingTime < step)
+                        Mylist[mx].WaitingTime = time - Mylist[mx].arrivalTime;
+                    if (time + Mylist[mx].ReminingTime < mn)
+                    {
+                        mn = time + Mylist[mx].ReminingTime;
+                        Mylist[mx].ReminingTime = 0.00001;
+                        Mylist[mx].FinishTime = mn;
+                        count++;
+                    }
+                    else
+                        Mylist[mx].ReminingTime -= mn - time;
+                    nslide.end = mn;
+                    time = mn;
+                    Final.Add(nslide);
+                    last = mx;
+                }
+            }
+            time = PTQ(time);
+        }
+        public void SRTN() //Preemptive Shortest Remaining Time Next (same algorithm like HPF but choose minimum RT
+        {
+            if (Final == null || Mylist == null)
+                return;
+            int count = 0, mx, last = -1;
+            Double time = 0.0, mn;
+            while (count < Mylist.Count)
+            {
+                mn = 10000.0;
+                mx = -1;
+                for (int i = 0; i < number; i++)
+                {
+                    if (Mylist[i].ReminingTime < step) continue;
+                    if (Mylist[i].arrivalTime > time)
+                    {
+                        mn = Mylist[i].arrivalTime;
+                        break;
+                    }
+                    if (mx == -1)
+                        mx = i;
+                    else if (Mylist[mx].ReminingTime > Mylist[i].ReminingTime)
+                        mx = i;
+                }
+                if (mx == -1)
+                {
+                    slides nslide = new slides();
+                    nslide.ID = 0;
+                    nslide.start = time;
+                    nslide.end = mn;
+                    time = mn;
+                    Final.Add(nslide);
+                    last = -1;
+                }
+                else
+                {
+                    if (last != mx && last != -1)
+                        time = PTQ(time);
+                    slides nslide = new slides();
+                    nslide.ID = Mylist[mx].ID;
+                    nslide.start = time;
+                    if (Mylist[mx].burstTime - Mylist[mx].ReminingTime < step)
+                        Mylist[mx].WaitingTime = time - Mylist[mx].arrivalTime;
+                    if (time + Mylist[mx].ReminingTime < mn)
+                    {
+                        mn = time + Mylist[mx].ReminingTime;
+                        Mylist[mx].ReminingTime = 0.00001;
+                        Mylist[mx].FinishTime = mn;
+                        count++;
+                    }
+                    else
+                        Mylist[mx].ReminingTime -= mn - time;
+                    nslide.end = mn;
+                    time = mn;
+                    Final.Add(nslide);
+                    last = mx;
+                }
+            }
+            time = PTQ(time);
+        }
+        public void FCFS() //first come first serve queue and add all process to it , start time of any one = max(finish time of the last +context time,arrival time)
+        {
+            if (Final == null || Mylist == null)
+                return;
+            Double time = 0.0;
+            for (int i = 0; i < number; i++)
+            {
+                slides nslid = new slides();
+                nslid.ID = Mylist[i].ID;
+                if (time < Mylist[i].arrivalTime)
+                    time = Mylist[i].arrivalTime;
+                nslid.start = time;
+                Mylist[i].WaitingTime = time - Mylist[i].arrivalTime;
+                time += Mylist[i].burstTime;
+                Mylist[i].FinishTime = time;
+                nslid.end = time;
+                Final.Add(nslid);
+                time = PTQ(time);
+            }
+
+        }
+        public void RR() //Round Robin with fixed time quantum
+        {
+            if (Final == null || Mylist == null)
+                return;
+            int count=0;
+            Double time = 0.0;
+            while (count < Mylist.Count)
+            {
+                Double mn = 10000.0;
+                //any process that already arrive will take min(TimeQuantum,ReminingTime)  
+                for (int i = 0; i < number; i++)
+                {
+                    if (Mylist[i].ReminingTime < step) continue;
+                    if (Mylist[i].arrivalTime < mn) mn = Mylist[i].arrivalTime;
+                    if (Mylist[i].arrivalTime > time) break;
+                    if (Mylist[i].burstTime - Mylist[i].ReminingTime < step) Mylist[i].WaitingTime = time-Mylist[i].arrivalTime;
+                    slides nslide = new slides();
+                    nslide.ID = Mylist[i].ID;
+                    nslide.start = time;
+                    if (Mylist[i].ReminingTime > TimeQuantum)
+                    {
+                        Mylist[i].ReminingTime -= TimeQuantum;
+                        time += TimeQuantum;
+                    }
+                    else
+                    {
+                        time += Mylist[i].ReminingTime;
+                        Mylist[i].ReminingTime=0.00001;
+                        Mylist[i].FinishTime = time;
+                        count++;
+                    }
+                    nslide.end = time;
+                    Final.Add(nslide);
+                    time = PTQ(time);
+                }
+                if (mn > time)
+                {
+                    slides nslide = new slides();
+                    nslide.ID = 0;
+                    nslide.start = time;
+                    nslide.end = mn;
+                    time = mn;
+                    Final.Add(nslide);
+                }
+            }
+           
+        }
+        
+        public bool Valid() //program will stop if any missing or wrong data
+        {
+            Double n;
+            if (FileName.Text == null || Context.Text == null || string.IsNullOrEmpty(Algo.Text) || Algo.SelectedIndex == -1 ||output.Text==null)
+                return false;
+            if (Algo.Text == "RR") //if RR has been choosen check for time quantum
+            {
+                if(Time.Text == null)
+                    return false;
+                if (!(Double.TryParse(Time.Text, out n)))
+                    return false;
+                if (Double.Parse(Time.Text) < step)
+                    return false;
+            }
+            if (!(Double.TryParse(Context.Text, out n)))
+                return false;
+            if (!System.IO.File.Exists(@FileName.Text) || !System.IO.File.Exists(@output.Text))
+                return false;
+            string[] lines = System.IO.File.ReadAllLines(@FileName.Text);
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (!Valid())
+                MessageBox.Show("missing or wrong data", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                
+                //*if no "." in the number add it , C# will make it 0 if there are no ".'
+                bool test = true;
+                for (int i = 0; i < Time.Text.Length; i++)
+                {
+                    if (Time.Text[i] == '.')
+                        test = false;
+                }
+                if (test)
+                    Time.Text += ".";
+                test = true;
+                for (int i = 0; i < Context.Text.Length; i++)
+                {
+                    if (Context.Text[i] == '.')
+                        test = false;
+                }
+                if (test)
+                    Context.Text += ".";
+                //*
+                string[] lines = System.IO.File.ReadAllLines(@FileName.Text);//read data from file
+                //assume all data is valid on folder
+                //initalize data
+                TimeQuantum = Double.Parse(Time.Text);
+                ContextSwitching = Double.Parse(Context.Text);
+                if(Mylist!=null)
+                    Mylist.Clear();
+                if(Final!=null)
+                    Final.Clear();
+                Mylist = new List<Process>();
+                Final = new List<slides>();
+                number = int.Parse(lines[0]);
+                for (int i = 1; i <= number; i++)
+                {
+                    Double[] values = lines[i].Split(' ').Select(Double.Parse).ToArray();
+                    Process curr=new Process();
+                    curr.ID=Convert.ToInt32(values[0]);
+                    //if any value less than zero take abslute
+                    if (values[1] < step)
+                        values[1] *= -1;
+                    if (values[2] < step)
+                        values[2] *= -1;
+                    curr.arrivalTime=values[1];
+                    curr.burstTime=curr.ReminingTime=values[2];
+                    curr.priority = Convert.ToInt32(values[3]);
+                    curr.WaitingTime = curr.FinishTime = -1.0;
+                    Mylist.Add(curr);
+                }
+                Mylist.Sort((x, y) => x.arrivalTime.CompareTo(y.arrivalTime));
+                //process with the given algorithm
+                if (Algo.Text == "HPF")
+                    HPF();
+                else if (Algo.Text == "FCFS")
+                    FCFS();
+                else if (Algo.Text == "RR")
+                    RR();
+                else if (Algo.Text == "SRTN")
+                    SRTN();
+
+                chart1_Click(sender, e); //draw chart
+                //print to file
+                Double sum1 = 0, sum2 = 0;
+                string line = "process ID \t Waiting time \t Turnaround time \t Weighted Turnaround time \r\n";
+                for (int i = 0; i < number; i++)
+                {
+                    string nLine = Mylist[i].ID.ToString() + "\t\t";
+                    nLine += Mylist[i].WaitingTime.ToString() + "\t\t";
+                    Double TAT = Mylist[i].FinishTime - Mylist[i].arrivalTime;
+                    sum1 += TAT;
+                    nLine += TAT.ToString() + " \t\t";
+                    Double ATAT = TAT / Mylist[i].burstTime;
+                    nLine += ATAT.ToString() + " \r\n";
+                    sum2 += ATAT;
+                    line += nLine;
+                }
+                sum1 /= number;
+                sum2 /= number;
+                line += "Average Turnaround time of the schedule " + sum1.ToString() + "\r\nAverage Weighted turnaround time of the schedule " + sum2.ToString();
+                System.IO.File.WriteAllText(@output.Text, line);
+            }
+        }
+        private void Algo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            if (Final == null || Final.Count() == 0)
+                return;
+            this.chart1.Series["process"].Points.Clear();
+            this.chart1.Series["process"].Points.AddXY(0.0,0);
+            for (int i = 0; i < Final.Count(); i++)
+            {
+                if (Final[i].end - Final[i].start < step)
+                    continue;
+                this.chart1.Series["process"].Points.AddXY(Final[i].start, Final[i].ID);
+                this.chart1.Series["process"].Points.AddXY(Final[i].end, Final[i].ID);
+            }
+        }
+
+        private void output_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
